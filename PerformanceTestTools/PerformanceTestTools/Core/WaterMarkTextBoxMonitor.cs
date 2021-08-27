c742be39310e52e0629e0dbe3ec10aab560bfabd
yanghj <yanghj@jiran.com>
2021-08-27 16:52:12 +0900

    기능을 구현하기 위한 UI/UX 완료

---
+++ PerformanceTestTools/PerformanceTestTools/Core/WaterMarkTextBoxMonitor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PerformanceTestTools.Core
+{
+    public class WaterMarkTextBoxMonitor : DependencyObject
+    {
+        public static bool GetIsMonitoring(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsMonitoringProperty);
+        }
+
+        public static void SetIsMonitoring(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsMonitoringProperty, value);
+        }
+
+        public static readonly DependencyProperty IsMonitoringProperty =
+            DependencyProperty.RegisterAttached("IsMonitoring", typeof(bool), typeof(WaterMarkTextBoxMonitor), new UIPropertyMetadata(false, OnIsMonitoringChanged));
+
+        public static bool GetWatermarkText(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(WatermarkTextProperty);
+        }
+
+        public static void SetWatermarkText(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkTextProperty, value);
+        }
+
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.RegisterAttached("WatermarkText", typeof(string), typeof(WaterMarkTextBoxMonitor), new UIPropertyMetadata(string.Empty));
+
+        public static int GetTextLength(DependencyObject obj)
+        {
+            return (int)obj.GetValue(TextLengthProperty);
+        }
+
+        public static void SetTextLength(DependencyObject obj, int value)
+        {
+            obj.SetValue(TextLengthProperty, value);
+
+            if (value >= 1)
+                obj.SetValue(HasTextProperty, true);
+            else
+                obj.SetValue(HasTextProperty, false);
+        }
+
+        public static readonly DependencyProperty TextLengthProperty =
+            DependencyProperty.RegisterAttached("TextLength", typeof(int), typeof(WaterMarkTextBoxMonitor), new UIPropertyMetadata(0));
+
+        public bool HasText
+        {
+            get { return (bool)GetValue(HasTextProperty); }
+            set { SetValue(HasTextProperty, value); }
+        }
+
+        private static readonly DependencyProperty HasTextProperty =
+            DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(WaterMarkTextBoxMonitor), new FrameworkPropertyMetadata(false));
+
+
+        static void OnIsMonitoringChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextBox txtBox = d as TextBox;
+
+            if ((bool)e.NewValue)
+                txtBox.TextChanged += TextChanged;
+            else
+                txtBox.TextChanged -= TextChanged;        
+        }
+
+        static void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox == null) return;
+            SetTextLength(txtBox, txtBox.Text.Length);
+        }
+    }
+}
--- PerformanceTestTools/PerformanceTestTools/MVVM/View/LoginPerformanceView.xaml.cs
+++ PerformanceTestTools/PerformanceTestTools/MVVM/View/LoginPerformanceView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -21,12 +25,34 @@ namespace PerformanceTestTools.MVVM.View
 
         private void ThreadCount_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
         private void ThreadCount_TextChanged(object sender, TextChangedEventArgs e)
         {
+            int ipno = 1;
+            try
+            {
+                ipno = Convert.ToInt32(ThreadCount.Text);
 
+                if (ipno > 2000)
+                {
+                    /// usercontrol은 부모 창에 패널로 붙어 있기 때문에 자신의 x,y 좌표를 가지고 있지 않다. 그렇기 때문에 화면의 중앙에 띄울수 있도록 함.
+                    MessageBox.Show("스레드 개수는 최대 2000개 이하여야 합니다.", "Thread Error", MessageBoxButton.OK);
+                    ThreadCount.Text = "2000";
+                }
+                else if (ipno < 1)
+                {
+                    MessageBox.Show("스레드 개수는 최소 1개 이어야 합니다.", "Thread Error", MessageBoxButton.OK);
+                    ThreadCount.Text = "1";
+                }
+            }
+            catch (FormatException)
+            {
+                ipno = 1;
+                ThreadCount.Text = "";
+            }
         }
 
         private void UpCount_Click(object sender, RoutedEventArgs e)
@@ -46,12 +72,21 @@ namespace PerformanceTestTools.MVVM.View
 
         private void startBtn_Click(object sender, RoutedEventArgs e)
         {
+            int threadcount = Int32.Parse(ThreadCount.Text);
+            string UID = UserID.Text;
+            string UPW = UserPW.Password;
+            MessageBox.Show("로그인 정보 : ID : " + UID + ", 암호 : " + UPW + ", 스레드 개수 : " + threadcount , "로그인", MessageBoxButton.OK);
+
+            // 스레드 갯수 만큼 스레드 제작
+
+            // 스레드 하나 하나 마다 통신 모듈 붙이기 dll을 붙여야 할거같은데...
 
         }
 
         private void Logview_Click(object sender, RoutedEventArgs e)
         {
-
+            string filePath = Directory.GetCurrentDirectory()+ "\\Log";
+            Process.Start("explorer.exe", filePath);
         }
     }
 }
